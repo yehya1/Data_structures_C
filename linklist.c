b305ac8f72aada9dec6bd7c2e77b7f2086303956
jscy <abohdaedyehya5@gmail.com>
2019-10-06 15:16:51 +0300

    add func to add elmint at index of the list

--- linklist.c
+++ linklist.c
@@ -57,7 +57,40 @@ void add_node_in_end(int n, node*& head, node*& tail )
 		tail->next = tmp;
 		tail = tail->next;
 	}
+}
+void print_in_index(int index, node*& haed) {
+	node *temp = haed;
+	int i;
+	for (i = 1;i != index && temp->next != NULL;i++)
+		temp = temp->next;
+	printf("%d\n",temp->data);
 
+}
+void add_node_in_index(int index,int n,node*& head, node*& tail) {
+	node *temp = head;
+	node *tmp = new node;
+	tmp->data = n;
+	tmp->next = NULL;
+	
+	if (head == NULL) {
+		tail = tmp;
+		head = tmp;
+	}
+	else {
+		if (index == 1) {
+			add_node_in_start(n, head, tail);
+			return;
+		}
+		for (int i = 2;i != index&&temp->next!=NULL;i++)
+			temp = temp->next;
+		if (temp->next == NULL)
+			add_node_in_end(n, head, tail);
+		else {
+			tmp->next = temp->next;
+			temp->next = tmp;
+		}
+
+	}
 
 }
 
@@ -69,6 +102,6 @@ int main()
 		add_node_in_start(i + 1, head, tail);
 		add_node_in_end(i + 1, head, tail);
 	}
+	add_node_in_index(2, 100, head, tail);
 	print_list(head);
-
 }
